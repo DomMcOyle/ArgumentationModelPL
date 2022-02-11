8f7a15f156f0ebd3ed73359931d6765f59eda4ab
Domenico Dell'Olio <zeito99@gmail.com>
2022-02-11 19:52:18 +0100

    aggiunti stub query da implementare

--- interface.pl
+++ interface.pl
@@ -14,6 +14,20 @@ get_input :- input_to_atom_list(A), get_input(A).
 get_input([quit]).
 get_input(Input) :- process_input(Input), input_to_atom_list(B), get_input(B).
 
+%process_input([show, relations]):-
 
 process_input([type, Lab]):- type(Lab, T), print(T).
-process_input([show]):-
+%process_input([show, testimonies]):-
+%process_input([show, values]):-
+%process_input([show, facts]):-
+%process_input([show, refences]):-
+%process_input([show, policies]):-
+
+%process_input([show, valuable]):-
+%process_input([is, ARG, argument]):-
+
+%process_input([show, arguments]):-
+%process_input([is, ARG, valuable]):-
+
+%process_input([get, reasons, A]):-
+%process_input([get, evidence, A]):-
