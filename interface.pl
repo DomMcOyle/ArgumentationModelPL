5e91e6a0d9d3eeaf0047d429b2e27ff8bfad1db8
Raffo99 <dis.raffaele99@gmail.com>
2022-02-14 12:37:09 +0100

    Refactor del codice

--- interface.pl
+++ interface.pl
@@ -1,77 +1,85 @@
+% Directives to the interpreter
 :- use_module('rec_eval.pl').
 :- dynamic loaded_kb/1.
 
+
+% Takes a list as unique argument and prints the concatenation of its content
 rec_print([]).
 rec_print([H|T]) :- write(H), rec_print(T).
 
+
+% Converts input in a list of atoms
 input_to_atom_list(AlistI) :-
     read_line_to_codes(user_input, Input),
 	string_to_atom(Input,IA),
 	atomic_list_concat(AlistI,' ',IA).
 
 
-
+% Main loop where is possible to call the defined commands without period
+% Appending a period to a command will result in the termination of the main loop
 main :- write('Insert absolute path of the KB: '), 
     read_line_to_codes(user_input, Input),
-	string_to_atom(Input,IA), 
+	string_to_atom(Input, IA), 
     consult(IA),
     assertz(loaded_kb(IA)),
 	get_input.
 
+
+% Handles all the input (Aquistions, conversion to atoms and execution)
 get_input :- write("Type a command: "), input_to_atom_list(A), get_input(A).
-
 get_input([quit]) :-loaded_kb(X), unload_file(X), retract(loaded_kb(X)).
-
 get_input(Input) :- process_input(Input), write("Type a command: "), input_to_atom_list(B), get_input(B).
 
-% relations command
 
+% Custom print for the relation command
 print_relations([]) :- write("Empty set\n"), !.
 print_relations([[A, B, C]|[]]) :- rec_print([C, ":", A, "->", B]), nl, !.
 print_relations([[A, B, C]|T]) :- rec_print([C, ":", A, "->", B]), nl, print_relations(T).
 
 
-
-% proposition labels command
-
+% Custom print for the proptext command
 print_proptext([]) :- write("Empty set\n"), !.
 print_proptext([[P, L]|[]]) :-  rec_print([L, ": ", P]), nl, !.
 print_proptext([[P, L]|T]) :- rec_print([L, ": ", P]), nl, print_proptext(T).
 
 
-% testimonies command
-
+% Custom print for list type
 print_list([]) :- write("Empty set\n"), !.
 print_list([H|[]]) :- rec_print([H, "\n"]), !. 
 print_list([H|T]):- rec_print([H, "\n"]), print_list(T).
 
-% valuables command
 
+% Custom print for the arguments
 print_arguments([]) :- write("Empty set.\n"), !.
 print_arguments([[R, E, C]|[]]) :- rec_print(["argument with conclusion: ", C, "\nhas relation set: ", R, "\nhas evidence set: ", E, "\n"]), !.
 print_arguments([[R, E, C]|T]) :- rec_print(["argument with conclusion: ", C, "\nhas relation set: ", R, "\nhas evidence set: ", E, "\n"]), print_arguments(T).
 
+
+% Converts the answer from its unified value to the atom 'yes', if the value is true, 'no' otherwise
 convert_answer(A, Value) :- (A -> Value = yes; Value = no).
 
+
+% Custom print for the propositions labels
 print_prop_labels([]) :- write("Empty set.\n"), !.
 print_prop_labels([H|[]]) :- label(L, H), rec_print([H, ": ", L]), nl, !.
 print_prop_labels([H|T]) :- label(L, H), rec_print([H, ": ", L]), nl, print_prop_labels(T).
 
 
-process_input([show, relations]):- findall([A, B, C], link(A, B, C), Xs), print_relations(Xs). %
-process_input([show, testimonies]) :- findall(A, type(A, testimony), Xs), print_list(Xs). %
-process_input([show, proptext]) :- findall([P, L], label(P, L), Xs), print_proptext(Xs). %
-process_input([type, Lab]):- type(Lab, T), print(T), nl. %
-process_input([show, values]) :- findall(A, type(A, value), Xs), print_list(Xs). %
-process_input([show, facts]):- findall(A, type(A, fact), Xs), print_list(Xs). %
-process_input([show, references]):- findall(A, type(A, reference), Xs), print_list(Xs). %
-process_input([show, policies]):- findall(A, type(A, policy), Xs), print_list(Xs). %
-process_input([show, evaluables]):- findall(A, rec_eval(A), Xs), print_arguments(Xs). %
-process_input([is, ARG, evaluable]):- term_string(X, ARG), convert_answer(rec_eval(X), Value), print(Value), nl.
-process_input([show, arguments]):- findall(A, argument(A), Xs), print_arguments(Xs). %
-process_input([is, ARG, argument]):- term_string(X, ARG), convert_answer(argument(X), Value), print(Value), nl.
-process_input([get, reasons, A]):- get_reasons(R, A), print_prop_labels(R). %
-process_input([get, evidences, A]):- get_evidences(E, A), print_prop_labels(E). %
+% Handles for processing inputs
+process_input([show, relations]):- findall([A, B, C], link(A, B, C), RelationsList), print_relations(RelationsList). %
+process_input([show, testimonies]) :- findall(A, type(A, testimony), TestimoniesList), print_list(TestimoniesList). %
+process_input([show, proptext]) :- findall([P, L], label(P, L), PropTextList), print_proptext(PropTextList). %
+process_input([type, Label]):- type(Label, LabelType), print(LabelType), nl. %
+process_input([show, values]) :- findall(A, type(A, value), ValuesList), print_list(ValuesList). %
+process_input([show, facts]):- findall(A, type(A, fact), FactsList), print_list(FactsList). %
+process_input([show, references]):- findall(A, type(A, reference), RefList), print_list(RefList). %
+process_input([show, policies]):- findall(A, type(A, policy), PoliciesList), print_list(PliciesList). %
+process_input([show, evaluables]):- findall(A, rec_eval(A), EvaluablesList), print_arguments(EvaluablesList). %
+process_input([is, ARG, evaluable]):- term_string(ConvertedARG, ARG), convert_answer(rec_eval(ConvertedARG), Value), print(Value), nl.
+process_input([show, arguments]):- findall(A, argument(A), ArgumentsList), print_arguments(ArgumentsList). %
+process_input([is, ARG, argument]):- term_string(ConvertedARG, ARG), convert_answer(argument(ConvertedARG), Value), print(Value), nl.
+process_input([get, reasons, A]):- get_reasons(ReasonsList, A), print_prop_labels(ReasonsList). %
+process_input([get, evidences, A]):- get_evidences(EvidencesList, A), print_prop_labels(EvidencesList). %
 process_input([help]) :- write("List of commands:
 -show relations: shows the complete list of relations between propositions in the loaded KB.
 -show proptext: shows the complete list of propositions codes with the corresponding sentence from the text.
@@ -94,9 +102,7 @@ N.B.: Argument must resemble the following structure: [R,E,C], where:
 \t-C is a conclusion.
 ").
 
-% code : 'predicate'
 
-%process_input([help]):-
-
+% Handles input that is not a recognized command
 process_input([_]):-write("command not recognized.\n"). 
 
--- rec_eval.pl
+++ rec_eval.pl
@@ -1,10 +1,11 @@
 :- module(rec_eval, [get_reasons/2, get_evidences/2, argument/1, rec_eval/1]).
 
-propositions(X) :- X = [testimony, value, policy, fact].
-evidence(X) :- X = [testimony, reference].
+propositions(PropositionsSet) :- PropositionsSet = [testimony, value, policy, fact].
+evidence(EvidenceSet) :- EvidenceSet = [testimony, reference].
 
-is_evidence(C) :- type(C, X), evidence(Y), member(X, Y).
-is_proposition(C) :- type(C, X), propositions(Y), member(X, Y).
+is_evidence(PossibleEvidence) :- type(PossibileEvidence, CorrespondingType), evidence(EvidenceSet), member(CorrespondingType, EvidenceSet).
+is_proposition(PossibileProposition) :- type(PossibileProposition, CorrespondingType), propositions(PropositionsSet), 
+                                        member(CorrespondingType, PropositionsSet).
 
 all_proposition([H|T]) :- is_proposition(H), all_proposition(T).
 all_proposition([]) :- !.
@@ -16,12 +17,12 @@ all_facts([H|T]) :- type(H, fact), all_facts(T).
 all_facts([]) :- !.
 
 % recover all the reasons for a given claim
-get_reasons(X, C) :- findall(Y, link(Y, C, reason), Z), same_set(X,Z), all_proposition(X).
+get_reasons(Reasons, Claim) :- findall(Y, link(Y, Claim, reason), ReasonsList), same_set(Reasons, ReasonsList), all_proposition(Reasons).
 % recover all the evidences for a given claim
-get_evidences(X, C) :- findall(Y, link(Y, C, evidence), Z), same_set(X,Z), all_evidence(X).
+get_evidences(Evidences, Claim) :- findall(Y, link(Y, Claim, evidence), EvidencesList), same_set(Evidences, EvidencesList), all_evidence(Evidences).
 
 % definition of argument
-argument(A) :- A = [R,E,C], is_proposition(C), get_reasons(R, C), get_evidences(E, C).
+argument(A) :- A = [R, E, C], is_proposition(C), get_reasons(R, C), get_evidences(E, C).
 
 % or clauses for the definition of evaluable
 evaluable(A) :- A = [_, _, C], argument(A), type(C, testimony).
@@ -33,20 +34,19 @@ evaluable(A) :- A = [R, _, C], argument(A), type(C, fact), length(R, L), L =\= 0
 evaluable(A) :- A = [R, E, C], argument(A), type(C, fact), length(R, L), L =\= 0, all_evidence(E).
 
 % definition of recursive evaluation
-rec_eval(A) :- evaluable(A), A = [[H|_], _, _], A1 = [_, _, H], argument(A1), rec_eval(A1).
+rec_eval(A) :- evaluable(A), A = [[H|_], _, _], SubA = [_, _, H], argument(SubA), rec_eval(SubA).
 rec_eval(A) :- evaluable(A), A = [[], _, _].
 
 % utilities
-same_set([H|T], C) :- member(H, C), remove(C,H,C2), same_set(T,C2), !.
+same_set([H|T], C) :- member(H, C), remove(C, H, C2), same_set(T, C2), !.
 same_set([],[]).
 
 remove([], _, []).
 remove([R|T], R, T).
 remove([H|T], R, [H|L]) :- R \= H, remove(T, R, L).
 
-
 not_member(_, []).
 not_member(X, [X|_]):- !, fail.
-not_member(X,[_|L]) :- not_member(X,L).
+not_member(X, [_|L]) :- not_member(X, L).
        
        
